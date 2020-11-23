ce499ebe7fa3fd9374dd969c31b37ce6212dece6
Upstate Networks Inc <info@upstatenetworks.com>
2020-11-23 10:36:10 -0500

    Main while loop for the overall process

--- emv.sh
+++ emv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #************************************
 #*  Bank of Utica Coffe EMV reader  *
-#* Rev 0.2 Upstate Networks Inc     *
+#* Rev 0.4 Upstate Networks Inc     *
 #************************************
 printf "EMV reader rev 0.2 %b\n"
 printf "Copyright Upstate Networks Inc 2020 %b\n"
@@ -11,42 +11,48 @@ printf "Copyright Upstate Networks Inc 2020 %b\n"
 #*  Loop forever until found          *
 #**************************************
 # LEDs= Device Status, Good card read, Error, MISC
+#Rev 3: main loop strings from scriptor
+while :
+do
+  a="$(lsusb | grep Cherry)"
+  echo $a
+  reader=""
+  echo "************"
+  reader="$(lsusb | grep Cherry)"
+  echo "Searching for EMV card reader"
+    while [ -z "$reader" ]
+      do
+        echo "EMV Card Reader not found.  Check connection"
+        reader="$(lsusb | grep Cherry)"
+        sleep 3
+      #Blink status LED
+    done
+  echo "Card Reader Found: " $reader
 
-a="$(lsusb | grep Cherry)"
-echo $a
-reader=""
-echo "************"
-reader="$(lsusb | grep Cherry)"
-echo "Searching for EMV card reader"
-while [ -z "$reader" ]
-  do
-    echo "EMV Card Reader not found.  Check connection"
-    reader="$(lsusb | grep Cherry)"
-    sleep 3
-    #Blink status LED
-done
-echo "Card Reader Found: " $reader
-
-echo "Coffee disabled, waiting for card insertion"
-#Loop waiting for card insertion
- b="$(pcsc_scan | grep Bank)"
-echo $b
-while [ -z "$b" ]
-  do
-    echo "Waiting for card insertion"
-    b="$(pcsc_scan)"
-    #sleep 1
-    #Blink ready LED
+  echo "Coffee disabled, waiting for card insertion"
+  #Loop waiting for card insertion
+  echo RESET | scriptor 
+ 
+  b="$(echo RESET | scriptor)"
+  echo $b
+  while [ -z "$b" ]
+    do
+      echo "Waiting for card insertion"
+      #sleep 1
+      b="$(echo RESET | scriptor)"
+      echo $b
+      #sleep 1
+      #Blink ready LED
   done
 
   echo "Coffee ready, please insert and lower lid"
-
-
-#while lsusb | grep Bank =/= null
-
     #check BID
     #Enable GPIO
     #enable other LEDs
-#end while
 
+    sleep 10
+    #reset system for next user
+
+#end while
+done
 #Log success
